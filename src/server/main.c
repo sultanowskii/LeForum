81531d71419b33cdbaf8874d13b6e40be6efb4c5
sultanowskii <dsr431@yandex.ru>
2022-03-15 00:56:25 +0300

    threaded socket server base

--- src/server/main.c
+++ src/server/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h> // i/o
 #include <stdlib.h> // default functions
 #include <string.h> // strings, memory
+#include <math.h>
+
+#include <unistd.h> // uni calls
 
 #include <pthread.h> // threading
 
@@ -8,8 +11,69 @@
 #include <arpa/inet.h> // inet_addr
 
 #include "lib/types.h"
+#include "lib/error.h"
+
+
+int32_t SERVER_PORT = 7431;
+char SERVER_ADDR[] = "0.0.0.0";
+int32_t MAX_CONNECTIONS = 100;
+
+
+void* handle_client(void* arg) {
+	char buffer[256];
+	int32_t fd = *(int32_t *)arg;
+	
+	sprintf(buffer, "Hi! fd=%d\n", fd);
+	printf("%s", buffer);
+	send(fd, buffer, strlen(buffer), 0);
+	close(fd);
+}
+
 
 int32_t main(int32_t argc, char* argv[]) {
-	puts("Hello, world!");
+	int32_t client_fd, server_fd;
+	struct sockaddr_in server_addr;
+	struct sockaddr client_addr;
+	socklen_t client_addr_len;
+	pthread_t client_thread;
+
+	puts("LeForum Server");
+
+	server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (server_fd < 0) {
+		perror("socket() failed");
+		return ERRCLIB;
+	}
+
+	server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(SERVER_PORT);
+
+	if (bind(server_fd, &server_addr, sizeof(server_addr)) != 0) {
+		perror("bind() failed");
+		return ERRCLIB;
+	}
+
+	if (listen(server_fd, MAX_CONNECTIONS) != 0) {
+		perror("listen() failed");
+		return ERRCLIB;
+	}
+
+	while (TRUE) {
+		client_fd = accept(server_fd, &client_addr, &client_addr_len);
+		
+		if (client_fd < 0) {
+			perror("accept() failed");
+			return ERRCLIB;
+		}
+		
+		// TODO: pass client_fd, client_addr, client_addr_len as arguments.
+		if (pthread_create(&client_thread, NULL, handle_client, (void*)&client_fd) != 0) {
+			perror("failed to create client handle");
+			return ERRCLIB;
+		}
+	}
+
 	return 0;
 }
