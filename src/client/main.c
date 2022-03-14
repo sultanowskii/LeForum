2edc26edfcf20b66d4ef867be53cba12675774f6
sultanowskii <dsr431@yandex.ru>
2022-03-14 23:34:12 +0300

    initial commit

---
+++ src/client/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h> // i/o
+#include <stdlib.h> // default functions
+#include <string.h> // strings, memory
+
+#include <pthread.h> // threading
+
+#include <sys/socket.h> // sockets
+#include <arpa/inet.h> // inet_addr
+
+#include "lib/types.h"
+
+int32_t main(int32_t argc, char* argv[]) {
+	puts("Hello, world!");
+	return 0;
+}
