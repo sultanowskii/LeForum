10d49281d25cf7e35c0f7e590023867bd4bb7dc2
sultanowskii <dsr431@yandex.ru>
2022-04-18 21:17:12 +0300

    CLI improvements, inner commands, prefixes

--- src/client/client.c
+++ src/client/client.c
@@ -19,6 +19,7 @@ const char *ServerCmdID_REPR(enum ServerCmdIDs id) {
 	switch (id) {
 		case scid_CONNECT_DISCONNECT:  return g_connected ? "Connect" : "Disconnect";
 		case scid_HISTORY:             return "Server history";
+		case scid_BACK:                return "Back";
 		default:                       return LESTATUS_NFND;
 	}
 };
@@ -28,12 +29,14 @@ const char *ThreadCmdID_REPR(enum ThreadCmdIDs id) {
 		case tcid_INFO:                return "Thread info";
 		case tcid_MESSAGES:            return "Message history";
 		case tcid_POST_MESSAGE:        return "Post message";
+		case tcid_BACK:                return "Back";
 		default:                       return LESTATUS_NFND;
 	}
 };
 
 const char *SettingsCmdID_REPR(enum SettingsCmdIDs id) {
 	switch (id) {
+		case stgcid_BACK:              return "Back";
 		default:                       return LESTATUS_NFND;
 	}
 };
@@ -42,28 +45,44 @@ status_t load_args(int argc, char **argv) {
 	argp_parse(&le_argp, argc, argv, 0, 0, &arguments);
 }
 
-inline void print_server_menu() {
-	for (enum MainCmdIDs scid = _scid_BEGIN + 1; scid < _scid_END; scid++) {
+inline void print_menu_server() {
+	for (enum ServerCmdIDs scid = _scid_BEGIN + 1; scid < _scid_END; scid++) {
 		printf("%d - %s\n", scid, ServerCmdID_REPR(scid));
 	}
 }
 
-inline void print_thread_menu() {
-	for (enum MainCmdIDs tcid = _tcid_BEGIN + 1; tcid < _tcid_END; tcid++) {
+inline void print_menu_thread() {
+	for (enum ThreadCmdIDs tcid = _tcid_BEGIN + 1; tcid < _tcid_END; tcid++) {
 		printf("%d - %s\n", tcid, ThreadCmdID_REPR(tcid));
 	}
 }
 
-inline void print_settings_menu() {
+inline void print_menu_settings() {
+	for (enum SettingsCmdIDs stgcid = _stgcid_BEGIN + 1; stgcid < _stgcid_END; stgcid++) {
+		printf("%d - %s\n", stgcid, SettingsCmdID_REPR(stgcid));
+	}
+}
+
+inline void print_menu_main() {
 	for (enum MainCmdIDs mcid = _mcid_BEGIN + 1; mcid < _mcid_END; mcid++) {
 		printf("%d - %s\n", mcid, MainCmdID_REPR(mcid));
 	}
 }
 
-inline void print_main_menu() {
-	for (enum MainCmdIDs stgcid = _stgcid_BEGIN + 1; stgcid < _stgcid_END; stgcid++) {
-		printf("%d - %s\n", stgcid, MainCmdID_REPR(stgcid));
-	}
+inline void print_prefix_server() {
+	printf("[Server] > ");
+}
+
+inline void print_prefix_thread() {
+	printf("[Thread] > ");
+}
+
+inline void print_prefix_settings() {
+	printf("[Settings] > ");
+}
+
+inline void print_prefix_main() {
+	printf("[*] > ");
 }
 
 status_t startup() {
@@ -79,11 +98,84 @@ void stop_program_handle(const int signum) {
 	g_working = FALSE;
 }
 
-status_t main(size_t argc, char **argv) {
-	char                tmp[128];
+void cmd_server() {
+	enum ServerCmdIDs   cmd_id = _scid_BEGIN;
+
+
+	while (cmd_id != scid_BACK) {
+		cmd_id = leclient_loop_process(print_menu_server, print_prefix_server);
+
+		switch (cmd_id) {
+			case scid_CONNECT_DISCONNECT:   return;
+			case scid_HISTORY:              return;
+			case scid_BACK:                 return;
+			default:                        puts("Command not found");
+		}
+	}
+}
+
+void cmd_thread() {
+	enum ThreadCmdIDs   cmd_id = _tcid_BEGIN;
+
+
+	while (cmd_id != tcid_BACK) {
+		cmd_id = leclient_loop_process(print_menu_thread, print_prefix_thread);
+
+		switch (cmd_id) {
+			case tcid_INFO:                 return;
+			case tcid_MESSAGES:             return;
+			case tcid_POST_MESSAGE:         return;
+			case tcid_BACK:                 return;
+			default:                        puts("Command not found");
+		}
+	}
+}
+
+void cmd_settings() {
+	enum SettingsCmdIDs cmd_id = _stgcid_BEGIN;
+
+
+	while (cmd_id != stgcid_BACK) {
+		cmd_id = leclient_loop_process(print_menu_settings, print_prefix_settings);
+
+		switch (cmd_id) {
+			case stgcid_BACK:               return;
+			default:                        puts("Command not found");
+		}
+	}
+	
+}
+
+void cmd_exit() {
+	cleanup();
+	exit(0);	
+}
+
+int leclient_loop_process(void (*print_menu)(), void (*print_prefix)()) {
+	static char         tmp[128];
 	size_t              tmp_size;
 
-	enum MainCmdIDs     section_id;
+	int                 cmd_id;
+
+
+	print_menu();
+	print_prefix();
+	
+	fgets(tmp, 127, stdin);
+	
+	tmp_size = strlen(tmp);
+	if (tmp_size >= 128) {
+		tmp_size = 127;
+	}
+	tmp[tmp_size] = '\0';
+
+	cmd_id = atoi(tmp);
+
+	return cmd_id;
+}
+
+status_t main(size_t argc, char **argv) {
+	enum MainCmdIDs     cmd_id;
 
 
 	load_args(argc, argv);
@@ -91,27 +183,13 @@ status_t main(size_t argc, char **argv) {
 	startup();
 
 	while (g_working) {
-		print_main_menu();
-		printf("[*] > ");
-		
-		fgets(tmp, 127, stdin);
-		
-		tmp_size = strlen(tmp);
-		if (tmp_size >= 128) {
-			tmp_size = 127;
-		}
-		tmp[tmp_size] = '\0';
+		cmd_id = leclient_loop_process(print_menu_main, print_prefix_main);
 
-		section_id = atoi(tmp);
-
-		printf("%d\n", section_id);
-
-		switch (section_id) {
-			// case mcid_SERVER:     cmd_server(); break;
-			// case mcid_THREAD:     cmd_thread(); break;
-			// case mcid_SETTINGS:   cmd_settings(); break;
-			// case mcid_EXIT:       cmd_exit(); break;
-			default:                  puts("Unknown command");
+		switch (cmd_id) {
+			case mcid_SERVER:     cmd_server(); continue;
+			case mcid_THREAD:     cmd_thread(); continue;
+			case mcid_SETTINGS:   cmd_settings(); continue;
+			case mcid_EXIT:       cmd_exit(); continue;
 		}
 	}
 
--- src/client/client.h
+++ src/client/client.h
@@ -19,7 +19,7 @@
 #include "client/arg.h"
 
 /**
- * @brief Main command list 
+ * @brief Main command list. 
  * 
  */
 enum MainCmdIDs {
@@ -32,18 +32,19 @@ enum MainCmdIDs {
 };
 
 /**
- * @brief Server command list 
+ * @brief Server command list. 
  * 
  */
 enum ServerCmdIDs {
 	_scid_BEGIN,
 	scid_CONNECT_DISCONNECT,
 	scid_HISTORY,
+	scid_BACK,
 	_scid_END,
 };
 
 /**
- * @brief Thread command list 
+ * @brief Thread command list. 
  * 
  */
 enum ThreadCmdIDs {
@@ -51,20 +52,22 @@ enum ThreadCmdIDs {
 	tcid_INFO,
 	tcid_MESSAGES,
 	tcid_POST_MESSAGE,
+	tcid_BACK,
 	_tcid_END,
 };
 
 /**
- * @brief Settings command list 
+ * @brief Settings command list.  
  * 
  */
 enum SettingsCmdIDs {
 	_stgcid_BEGIN,
+	stgcid_BACK,
 	_stgcid_END,
 };
 
 /**
- * @brief Returns string representation of main command with provided id
+ * @brief Returns string representation of main command with provided id. 
  * 
  * @param id Command ID
  * @return Command string representation. LESTATUS_NFND is returned if there is no such command with given id
@@ -72,7 +75,7 @@ enum SettingsCmdIDs {
 const char *MainCmdID_REPR(enum MainCmdIDs id);
 
 /**
- * @brief Returns string representation of server command with provided id
+ * @brief Returns string representation of server command with provided id. 
  * 
  * @param id Command ID
  * @return Command string representation. LESTATUS_NFND is returned if there is no such command with given id
@@ -80,7 +83,7 @@ const char *MainCmdID_REPR(enum MainCmdIDs id);
 const char *ServerCmdID_REPR(enum ServerCmdIDs id);
 
 /**
- * @brief Returns string representation of thread command with provided id
+ * @brief Returns string representation of thread command with provided id. 
  * 
  * @param id Command ID
  * @return Command string representation. LESTATUS_NFND is returned if there is no such command with given id
@@ -88,7 +91,7 @@ const char *ServerCmdID_REPR(enum ServerCmdIDs id);
 const char *ThreadCmdID_REPR(enum ThreadCmdIDs id);
 
 /**
- * @brief Returns string representation of settings command with provided id
+ * @brief Returns string representation of settings command with provided id. 
  * 
  * @param id Command ID
  * @return Command string representation. LESTATUS_NFND is returned if there is no such command with given id
@@ -97,14 +100,14 @@ const char *SettingsCmdID_REPR(enum SettingsCmdIDs id);
 
 
 /**
- * @brief Initialises program 
+ * @brief Initialises program, 
  * 
  * @return LESTATUS_OK on success 
  */
 status_t startup();
 
 /**
- * @brief Cleans all the program data 
+ * @brief Cleans all the program data. 
  * 
  * @return LESTATUS_OK on success 
  */
@@ -113,32 +116,89 @@ status_t cleanup();
 /**
  * @brief Program termination handler. Simply modifies g_working value. 
  * 
- * @param Singnum
+ * @param Signum
  */
 void stop_program_handle(const int signum);
 
 /**
- * @brief Prints server menu
+ * @brief Prints server menu. 
  * 
  */
-void print_server_menu();
+void print_menu_server();
 
 /**
- * @brief Prints thread menu
+ * @brief Prints thread menu. 
  * 
  */
-void print_thread_menu();
+void print_menu_thread();
 
 /**
- * @brief Prints settings menu
+ * @brief Prints settings menu. 
  * 
  */
-void print_settings_menu();
+void print_menu_settings();
 
 /**
- * @brief Prints main menu 
+ * @brief Prints main menu. 
  * 
  */
-void print_main_menu();
+void print_menu_main();
+
+/**
+ * @brief Prints server prefix before user input
+ * 
+ */
+void print_prefix_server();
+
+/**
+ * @brief Prints thread prefix before user input
+ * 
+ */
+void print_prefix_thread();
+
+/**
+ * @brief Prints settings prefix before user input
+ * 
+ */
+void print_prefix_settings();
+
+/**
+ * @brief Prints main prefix before user input
+ * 
+ */
+void print_prefix_main();
+
+/**
+ * @brief Processes Server commands. 
+ * 
+ */
+void cmd_server();
+
+/**
+ * @brief Processes Thread commands.
+ * 
+ */
+void cmd_thread();
+
+/**
+ * @brief Processes Settings commands. 
+ * 
+ */
+void cmd_settings();
+
+/**
+ * @brief Processes Exit commands. 
+ * 
+ */
+void cmd_exit();
+
+/**
+ * @brief Prints all the menues and stuff, then reads command from user. 
+ * 
+ * @param print_menu Function that prints menu
+ * @param print_prefix Function that prints prefix before user input
+ * @return User command
+ */
+int leclient_loop_process(void (*print_menu)(), void (*print_prefix)());
 
 status_t main(size_t argc, char **argv);
